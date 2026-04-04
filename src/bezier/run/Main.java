d3962d45f3fb06f64a12c29bc97d91227f6553d4
Nael <nael.bensaadi9@etu.univ-lorraine.fr>
2026-04-04 13:48:49 +0200

    Mise à jour de la logique d'allocation dans MirrorSampling

--- src/bezier/run/Main.java
+++ src/bezier/run/Main.java
@@ -41,8 +41,8 @@ public final class Main extends OutputWriter {
 
 	private static final String LOG_FILE = "bezier.log";
 	private static final int AWAIT = 1;
-	//private static final int NB_RUNS = 10;
-	private static final int NB_RUNS = 3;
+	private static final int NB_RUNS = 10;
+	//private static final int NB_RUNS = 3;
 	private static final int NB_SECONDS = 60;
 	// private static final int NB_SECONDS = 10;
 	public static final boolean DISPLAY_CHART = true;
@@ -54,8 +54,8 @@ public final class Main extends OutputWriter {
 
 	// ====== CHOIX DES PROBLEMES A LANCER ======
 	// Mettre les noms des probs voulus (ex: {"prob4"} pour prob4 seul)
-	//private static final String[] SELECTED_PROBLEMS = {"prob1", "prob2", "prob3", "prob4", "prob5", "prob6", "prob7", "prob8", "prob9", "prob10", "prob11"};
-	private static final String[] SELECTED_PROBLEMS = {"prob9"};
+	private static final String[] SELECTED_PROBLEMS = {"prob1", "prob2", "prob3", "prob4", "prob5", "prob6", "prob7", "prob8", "prob9", "prob10", "prob11"};
+	//private static final String[] SELECTED_PROBLEMS = {"prob9"};
 	/**
 	 * @return Retourne l'instance de Main
 	 */
